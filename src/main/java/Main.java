1f1193c1ffa5a4012826b3609b4b4daa9e5c4408
Ramzin Nikita <rivorn73@gmail.com>
2022-04-01 01:56:33 +0500

    Study

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -19,18 +20,41 @@ public class Main {
         element.click();
 
         WebElement button = (new WebDriverWait(driver, Duration.ofSeconds(10))).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[contains(text(),'Для бизнеса')]")));// явное ожидание
+
+
         System.out.println(button.getAttribute("baseURI"));//Работа с properties атрибута
         button.click();
 
 
         button = driver.findElement(By.xpath("//p[@class='hero-fb--description']"));
-
         System.out.println(button.getText());//Вывод текста из атрибута
 
+
+
         driver.get("https://google.com");
-
         WebElement input = driver.findElement(By.xpath("//input[@aria-label='Найти']"));
-
         input.sendKeys("Летов", Keys.ENTER);//Ввод текста и поиск. Вместо текста можно передать путь к файлу
+        //input.clear();//очистить поле ввода
+
+
+
+
+       // Actions actions =new Actions();
+        /*actions.contextClick()//Клик правой кнопкой мыши
+
+        actions.moveToElement()//навести на какой-то элемент
+        actions.keyDown()//нажали клавишу
+        actions.keyUp()//нажали отпустили клавишу
+        actions.clickAndHold()//нажать и удержать
+        actions.release()//отпустить при удержании
+
+        actions.build()//собрать все действия выше в одно
+        actions.perform();//применить все действия выше*/
+            driver.get("https://mrdoob.com/projects/chromeexperiments/google-space/");
+            WebElement element1 = driver.findElement(By.xpath("//img"));
+            WebElement element2 = driver.findElement(By.xpath("//a [contains(text(),'Advertising Programs')]"));
+            Actions actions =new Actions(driver);
+            actions.moveToElement(element1).clickAndHold().moveToElement(element2).build().perform();
+
     }
 }
